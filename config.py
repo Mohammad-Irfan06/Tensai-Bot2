55de4b3514b3a62c65574c64d817183db16660c0
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-04 21:19:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+import os
 
+API_ID = int(os.environ.get("API_ID", ""))  # Replace with your API_ID
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+MONGO_URL = os.environ.get("MONGO_URL", "")  # MongoDB URI
