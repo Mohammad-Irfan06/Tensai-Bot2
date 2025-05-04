58d56a387d9a3d3d6e8cd190f743fcfba5d1f8fc
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-04 21:23:32 +0530

    Update start.py

--- handler/start.py
+++ handler/start.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command("start"))
+async def start(bot, message):
+    user_id = message.from_user.id
+    welcome_text = f"Hello, {message.from_user.first_name}!\n\nWelcome to the Telegram Rename Bot. \nUse the bot to rename your files and add custom thumbnails. Just send a file and follow the prompts."
+    
+    await message.reply_text(welcome_text)
