073b59715f6c743329ba2a6d05b5f15950a5e795
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-04 21:27:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
-from pyrogram import Client
-from pyrogram.types import BotCommand
-from config import API_ID, API_HASH, BOT_TOKEN
-from handler import start, callback
-from plugins import rename
+import os
+import time
+import logging
+from pyrogram import Client, filters
+from helper.database import get_thumbnail
+from helper.utils import progress_for_pyrogram
+from pyrogram.errors import FloodWait
+from handler.callback import cancel, rename, upload_file
 
-bot = Client(
-    "rename-bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins={"root": "plugins"}
-)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("RenameBot")
 
-async def set_commands():
-    await bot.set_bot_commands([
-        BotCommand("start", "Start the bot"),
-        BotCommand("help", "How to use the bot"),
-    ])
+API_ID = "YOUR_API_ID"
+API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
 
-if __name__ == "__main__":
-    bot.run(set_commands())
-    bot.run()
+bot = Client("rename_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+bot.add_handler(cancel)
+bot.add_handler(rename)
+bot.add_handler(upload_file)
+
+bot.run()
