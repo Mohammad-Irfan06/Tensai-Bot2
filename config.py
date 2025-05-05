54295602ecda8523a2ea938c3511b3ed390b964a
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-05 15:20:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 MONGO_DB_URI = os.getenv("MONGO_DB_URI")
-FFMPEG_PATH = os.getenv("FFMPEG_PATH", "/usr/bin/ffmpeg")  # default path for ffmpeg
+FFMPEG_PATH = os.getenv("FFMPEG_PATH", "/usr/bin/ffmpeg")
 
 # Temporary Download Path
 DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "./downloads")
+
+# Welcome Image Path
+WELCOME_IMAGE = os.getenv("WELCOME_IMAGE", "./resources/start_pic.jpg")
