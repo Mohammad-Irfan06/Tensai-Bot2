b7628a8b8052801ed4876d142302c92d3c3d5ec5
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-05 15:31:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 API_ID = int(os.getenv("21786970"))
-API_HASH = os.getenv("aa1eaa84080fdf706c5cb37a27d35e81)
+API_HASH = os.getenv("aa1eaa84080fdf706c5cb37a27d35e81")
 BOT_TOKEN = os.getenv("7849794082:AAEhUjqhZS-YMrWKgWghLbZnivv3MnRR_Lo")
 
-MONGO_DB_URl = os.getenv("mongodb+srv://mohammadirfan5227:uwkiGejZGyRLV3nP@cluster0.ub1brgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = os.getenv("mongodb+srv://mohammadirfan5227:uwkiGejZGyRLV3nP@cluster0.ub1brgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 FFMPEG_PATH = os.getenv("FFMPEG_PATH", "/usr/bin/ffmpeg")
 
 # Temporary Download Path
@@ -12,3 +12,7 @@ DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "./downloads")
 
 # Welcome Image Path
 WELCOME_IMAGE = os.getenv("WELCOME_IMAGE", "./resources/start_pic.jpg")
+
+# Owner and Force Subscribe Channel
+OWNER_ID = int(os.getenv("5944299635"))  # Telegram user ID of the bot owner
+FORCE_SUB_CHANNEL = os.getenv("KMovieHubHindi")  # username of your updates channel without @
