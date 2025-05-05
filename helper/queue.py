3ce46c7663386c009836d7a984c1211d3106c905
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-05 12:40:15 +0530

    Update queue.py

--- helper/queue.py
+++ helper/queue.py
@@ -1,1 +1,14 @@
+import asyncio
+from asyncio import Queue
 
+file_queue = Queue()
+
+async def process_file_queue():
+    while True:
+        file_data = await file_queue.get()
+        # Process file (e.g., renaming and thumbnail embedding)
+        await process_file(file_data)
+        file_queue.task_done()
+
+async def add_to_queue(file_data):
+    await file_queue.put(file_data)
