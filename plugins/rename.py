eb736938db4d0f0fe084d1b1c6befc95083c5ee1
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-05 12:41:38 +0530

    Update rename.py

--- plugins/rename.py
+++ plugins/rename.py
@@ -1,51 +1,47 @@
+import os
+import ffmpeg
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from helper.database import save_thumbnail
-import os
-import logging
+from helper.database import save_file_path
+from helper.utils import sanitize_filename
+from helper.queue import add_to_queue
 
-logger = logging.getLogger(__name__)
+rename_handler = Client.on_message(filters.document)
 
-@Client.on_message(filters.command("set_thumbnail"))
-async def set_thumbnail(bot: Client, message: Message):
-    user_id = message.from_user.id
-    if message.reply_to_message and message.reply_to_message.photo:
-        # Save thumbnail in DB
-        file_id = message.reply_to_message.photo.file_id
-        thumbnail_path = await bot.download_media(file_id)
-        
-        # Save path in database
-        await save_thumbnail(user_id, thumbnail_path)
-        await message.reply("Thumbnail has been set successfully!")
-    else:
-        await message.reply("Please reply to a photo to set it as a thumbnail.")
-
-@Client.on_message(filters.command("rename"))
-async def rename(bot: Client, message: Message):
-    user_id = message.from_user.id
-    if message.reply_to_message:
-        new_filename = message.text.split(" ", 1)[1].strip()  # Extract new filename from the message
-        
-        if not new_filename:
-            await message.reply("Please provide a new filename.")
-            return
-        
-        file = message.reply_to_message
-        file_path = await bot.download_media(file)
-        new_file_path = os.path.join("/tmp", new_filename)
-        
-        try:
-            os.rename(file_path, new_file_path)
-            # If thumbnail exists, embed it
-            thumbnail_path = await get_thumbnail(user_id)
-            thumb_dl_path = thumbnail_path if thumbnail_path else None
-
-            await bot.send_document(
-                chat_id=user_id,
-                document=new_file_path,
-                caption=f"Renamed file: `{new_filename}`",
-                thumb=thumb_dl_path,
+async def rename_handler(client: Client, message: Message):
+    user_id = str(message.from_user.id)
+    
+    # Download the file
+    file_name = sanitize_filename(message.document.file_name)
+    temp_file_path = os.path.join("/tmp", f"{user_id}_{file_name}")
+    
+    await message.download(file_name=temp_file_path)
+    
+    # Get the thumbnail if available
+    thumb_path = os.path.join("/tmp", f"{user_id}_thumb.jpg")
+    if message.photo:
+        await message.download(file_name=thumb_path)
+    
+    # Process the video: Rename and embed thumbnail
+    new_file_path = os.path.join("/tmp", f"renamed_{file_name}")
+    try:
+        if os.path.exists(thumb_path):
+            # Embedding thumbnail using FFmpeg
+            (
+                ffmpeg
+                .input(temp_file_path)
+                .output(new_file_path, vcodec="copy", acodec="copy", 
+                        map="0", attach=thumb_path)
+                .run(overwrite_output=True)
             )
-            os.remove(new_file_path)
-        except Exception as e:
-            await message.reply(f"Error during renaming: {e}")
+        else:
+            os.rename(temp_file_path, new_file_path)
+        
+        # Save file path to MongoDB
+        await save_file_path(user_id, new_file_path)
+        
+        # Send the renamed video with thumbnail (if attached)
+        await message.reply_video(video=new_file_path, caption="Here is your renamed video!")
+        
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
