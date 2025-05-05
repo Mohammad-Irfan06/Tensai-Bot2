eb76bcc3e31410639c33d2447e2ac6c347f57f6b
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-05 12:44:44 +0530

    Update thumb.py

--- plugins/thumb.py
+++ plugins/thumb.py
@@ -1,1 +1,18 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+thumb_handler = Client.on_message(filters.photo)
+
+async def thumb_handler(client: Client, message: Message):
+    user_id = str(message.from_user.id)
+    
+    # Save the thumbnail sent by the user
+    thumb_path = os.path.join("downloads", f"{user_id}.jpg")
+    
+    # Download the thumbnail image
+    await message.download(file_name=thumb_path)
+    
+    # Confirm that the thumbnail is saved
+    await message.reply_text("Thumbnail saved successfully!")
 
