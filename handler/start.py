6e80217159f18de2a07c686d5312c48e590acdc3
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-09 19:07:48 +0530

    Update start.py

--- handler/start.py
+++ handler/start.py
@@ -3,14 +3,17 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import WELCOME_IMAGE
 
-start_handler = Client.on_message(filters.command("start"))
-
-@start_handler
+# This handler will respond to the /start command
+@Client.on_message(filters.command("start"))
 async def start(client: Client, message: Message):
+    # Send the welcome image with a caption and inline buttons
     await message.reply_photo(
         photo=WELCOME_IMAGE,
         caption="Welcome to the Tensai Rename Bot! 🎬\n\nSend me any video or file to rename it.",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Help", callback_data="how_to_use")]]
+            [
+                [InlineKeyboardButton("Help", callback_data="how_to_use")],
+                [InlineKeyboardButton("About", callback_data="about")]
+            ]
         )
     )
