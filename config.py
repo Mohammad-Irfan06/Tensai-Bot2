203210d40f5b360a858e4eed42ef94b56a7d22f5
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-09 20:08:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = 21786970  # Use the integer value directly
 API_HASH = "aa1eaa84080fdf706c5cb37a27d35e81"
 BOT_TOKEN = "7849794082:AAEhUjqhZS-YMrWKgWghLbZnivv3MnRR_Lo"
 
-MONGO_DB_URI = "mongodb+srv://mohammadirfan5227:uwkiGejZGyRLV3nP@cluster0.ub1brgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URI = "mongodb+srv://mohammadirfan01239:T1njoUYWxmsu1itN@cluster0.suyfipt.mongodb.net/TensaiRenameBot?retryWrites=true&w=majority&appName=Cluster0"
 FFMPEG_PATH = "/usr/bin/ffmpeg"  # Path to ffmpeg
 
 # Temporary Download Path
