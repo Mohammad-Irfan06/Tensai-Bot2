f32a741f9433a49a2b1639b6e938a7c629d5f983
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-09 20:13:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,12 @@ API_ID = 21786970  # Use the integer value directly
 API_HASH = "aa1eaa84080fdf706c5cb37a27d35e81"
 BOT_TOKEN = "7849794082:AAEhUjqhZS-YMrWKgWghLbZnivv3MnRR_Lo"
 
-MONGO_DB_URI = "mongodb+srv://mohammadirfan01239:T1njoUYWxmsu1itN@cluster0.suyfipt.mongodb.net/TensaiRenameBot?retryWrites=true&w=majority&appName=Cluster0"
+# MongoDB connection details
+MONGO_DB_URI = "mongodb+srv://mohammadirfan5227:uwkiGejZGyRLV3nP@cluster0.ub1brgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "TensaiRenameBot"  # Database name
+COLLECTION_NAME = "botUsers"  # Collection name for bot users
+
+# FFmpeg path
 FFMPEG_PATH = "/usr/bin/ffmpeg"  # Path to ffmpeg
 
 # Temporary Download Path
@@ -13,4 +18,3 @@ DOWNLOAD_DIR = "./downloads"
 
 # Welcome Image Path
 WELCOME_IMAGE = "./resources/start_pic.jpg"
-
