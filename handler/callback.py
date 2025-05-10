2ce46895ea2e4df27e8310b1e1c9a3a9ef522fa2
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-10 12:08:57 +0530

    Update callback.py

--- handler/callback.py
+++ handler/callback.py
@@ -1,6 +1,12 @@
-from pyrogram import Client
-from pyrogram.types import CallbackQuery
+from helper.queue import process_queue
+from utils.progress import display_progress
 
-async def callback_handler(client: Client, callback_query: CallbackQuery):
-    # Handle callback query from inline buttons (if any)
-    await callback_query.answer("Callback received!")
+async def handle_callback(data):
+    file_id = data['file_id']
+    user_id = data['user_id']
+
+    # Show progress while renaming
+    for percent in range(0, 101, 10):
+        display_progress(percent, current=percent, total=100, speed='500KB', time_remaining='5s')
+
+    await process_queue(file_id, user_id)
