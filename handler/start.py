2f5b362beb0f21d4111e8857e9abb6578e217aa0
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-10 12:11:10 +0530

    Update start.py

--- handler/start.py
+++ handler/start.py
@@ -1,7 +1,4 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from config import WELCOME_IMAGE
+import logging
 
-@Client.on_message(filters.command("start"))
-async def start(client: Client, message: Message):
-    await message.reply_photo(photo=WELCOME_IMAGE, caption="Welcome to the Tensai Rename Bot!")
+def start_bot():
+    logging.info("🤖 Tensai Rename Bot Started Successfully!")
