893e37e2be88ab4306a110bb91f878abc76266be
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-10 12:13:12 +0530

    Update progress.py

--- plugins/progress.py
+++ plugins/progress.py
@@ -1,3 +1,10 @@
+import time
+
+def track_progress():
+    for percent in range(0, 101, 10):
+        time.sleep(1)
+        print(f"Processing... {percent}% Done!")
+
 def display_progress(percent, current, total, speed, time_remaining):
     progress_bar = "⭐" * (percent // 10) + "☆" * (10 - percent // 10)
     print(f"""
