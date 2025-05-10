e5f748ec2ab67bc98fded48c67b9d1bf399b138c
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-10 12:08:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,9 @@
-import os
-
-# Directly set your credentials here
-API_ID = 21786970  # Use the integer value directly
+# API Credentials
+API_ID = 21786970
 API_HASH = "aa1eaa84080fdf706c5cb37a27d35e81"
 BOT_TOKEN = "7629995937:AAGKZjjmPrg943J44zqd1Sj3T-Y5x5NzSGY"
 
-# MongoDB connection details
+# MongoDB Connection
 MONGO_DB_URI = "mongodb+srv://mohammadirfan5227:uwkiGejZGyRLV3nP@cluster0.ub1brgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DB_NAME = "TensaiRenameBot"  # Database name
-COLLECTION_NAME = "botUsers"  # Collection name for bot users
-
-# FFmpeg path
-FFMPEG_PATH = "/usr/bin/ffmpeg"  # Path to ffmpeg
-
-# Temporary Download Path
-DOWNLOAD_DIR = "./downloads"
-
-# Welcome Image Path
-WELCOME_IMAGE = "./resources/start_pic.jpg"
+DB_NAME = "TensaiRenameBot"
+COLLECTION_NAME = "botUsers"
