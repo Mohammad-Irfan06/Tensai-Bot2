4b01cd0381b9f967dc634f3bd2cf328a31776288
Mohammad-Irfan06 <mohammad.irfan5227@gmail.com>
2025-05-23 08:49:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # API Credentials
 API_ID = 21786970
 API_HASH = "aa1eaa84080fdf706c5cb37a27d35e81"
-BOT_TOKEN = "7629995937:AAGKZjjmPrg943J44zqd1Sj3T-Y5x5NzSGY"
+BOT_TOKEN = "7629995937:AAFNwMsOI_PAV91BPq4lnQX9t_w1xxkDd8Q"
 
 # MongoDB Connection
 MONGO_DB_URI = "mongodb+srv://mohammadirfan5227:uwkiGejZGyRLV3nP@cluster0.ub1brgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
