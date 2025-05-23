24d8fb2b7222ebaf09cdf6d8cf52eba399efb30a
Mohammad-Irfan06 <mohammad.irfan5227@gmail.com>
2025-05-23 08:47:31 +0530

    Update thumb.py

--- plugins/thumb.py
+++ plugins/thumb.py
@@ -24,7 +24,7 @@ def attach_thumbnail(video_file, thumbnail_file):
         output_path = f"embedded_{os.path.basename(video_file)}"
 
         # Use ffmpeg-python to embed the thumbnail into the video
-        ffmpeg.input(temp_thumb, loop=1).output(output_path, map="0", map="1", c="copy", dis="1").run()
+        ffmpeg.input(temp_thumb, loop=1).output(output_path, map=["0", "1"], c="copy", dis="1").run()
 
         # Cleanup the temporary thumbnail file
         os.remove(temp_thumb)
